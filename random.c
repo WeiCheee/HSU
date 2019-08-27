9864105486fdcc457ee679853bf1d67b05c21105
WeiCheee <davidhsu960307@gmail.com>
2019-08-27 14:07:17 +0800

    random1 random.c

--- random.c
+++ random.c
@@ -1,6 +1,24 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
 int main(){
-    printf("hello word");
+
+    srand(time(0));
+    int i, j, isused, n[10];
+    for(i=1; i<=10; i++){
+        do{
+            n[i-1] = rand() % 10 + 1;
+            //isused = 0;
+            for(j=1; j<i; j++){
+               if(n[i-1] == n[j-1]){
+                    //isused = 1;
+                    break;
+               }
+            }
+        }while(i!=j);
+        printf("%d\n", n[i-1]);
+    }
+
+    return 0;
 }
